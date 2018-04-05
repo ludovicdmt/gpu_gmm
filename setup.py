8766fe29eb20f42688bc0dac46247130bbe1ffd1
Unknown <ludovic.darmet@gmail.com>
2018-04-05 13:33:16 +0200

    error in dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='gpu_gmm',
-      version='0.1',
+      version='0.2',
       description='Online Gaussian Mixture and on GPU',
       url='Not yet on git',
       author='Ludovic DARMET',
       author_email='ludovic.darmet@gipsa-lab.fr',
       packages=['gpu_gmm'],
       install_requires=[
-          'opencv-python', 'sklearn', 'tensorflow'
+          'numpy', 'sklearn', 'tensorflow'
       ],
       zip_safe=False)
